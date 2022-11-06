9f87ffd33b3fc44029f3d48d4f2a622528810249
Piku Maity <87698874+picoders1@users.noreply.github.com>
2022-11-06 15:59:28 +0530

    Java Servlet Files

---
+++ CancleOrderServlet.java
@@ -0,0 +1,35 @@
+package maakaliflorist.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import maakaliflorist.connection.DBcon;
+import maakaliflorist.bt.Orderbt;
+
+@WebServlet("/cancel-order")
+public class CancleOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try(PrintWriter out = response.getWriter()) {
+			String id = request.getParameter("id");
+			if(id != null) {
+				Orderbt orderDao = new Orderbt(DBcon.getConnection());
+				orderDao.cancelOrder(Integer.parseInt(id));
+			}
+			response.sendRedirect("orders.jsp");
+		} catch (ClassNotFoundException|SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+}
